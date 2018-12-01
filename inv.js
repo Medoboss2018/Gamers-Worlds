509ca1a1a72953bd8f0fa5cbb0bba1d31223652f
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-01 19:12:09 +0200

    Update inv.js

--- inv.js
+++ inv.js
@@ -1,27 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
  client.on('message', message => {
-    if (!message.guild) return;
-    if (message.content.startsWith("رابط"})    {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
 
-
-message.channel.createInvite({
- thing: true,
- maxUses: 1,
- maxAge: 86400
-}).then(invite=>
- message.author.sendMessage(invite.url)
-}
-message.channel.send(`**تم ارسال الرابط برسالة خاصة**`)
-message.author.send(`**هدا الرابط لشخص و لمدة 24 ساعة**`)
-}
-});
-(edited)
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
 
 client.on('message', message => {
 var prefix = "$";
