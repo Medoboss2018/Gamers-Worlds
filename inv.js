514d8fc7bb4f05864dda1c28608c47448178e123
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-06 12:48:23 +0200

    Update inv.js

--- inv.js
+++ inv.js
@@ -48,4 +48,22 @@ message.channel.sendMessage(`**${argresult}**: تم تغير الاستماع ا
 
 });
 
+client.on('message',async message => {
+  if(message.content.startsWith("$setvoice")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Network Gamers Voice : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+    },1000);
+  });
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
